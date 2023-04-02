2241a24bad8c0c2a3851ec368738aa12854ea5e5
First Fractal <firstfractal512@gmail.com>
2023-04-02 15:22:40 -0500

    made it always a blood moon

--- AlwaysBloodMoon.cs
+++ AlwaysBloodMoon.cs
@@ -1,3 +1,4 @@
+using Terraria;
 using Terraria.ModLoader;
 
 namespace AlwaysBloodMoon
@@ -5,4 +6,19 @@ namespace AlwaysBloodMoon
 	public class AlwaysBloodMoon : Mod
 	{
 	}
+
+	public class AlwaysBloodMoonSystem: ModSystem
+	{
+        public override void PreUpdateWorld()
+        {
+            if (Main.dayTime == false)
+            {
+                Main.bloodMoon = true;
+            } else
+            {
+                Main.bloodMoon = false;
+            }
+            base.PreUpdateWorld();
+        }
+    }
 }
